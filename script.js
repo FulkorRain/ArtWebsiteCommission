3bf704d51787563cb2e179b349279f618eceae53
FulkorRain <ranygamer353@gmail.com>
2025-12-21 23:00:32 +0300

    Added basic linkme button

---
+++ script.js
@@ -0,0 +1,7 @@
+function copyLink() {
+  const code = `<a href="https://fulkorrain.github.io/ArtWebsiteCommission/">
+  <img src="https://cdn.discordapp.com/attachments/1447239459539255440/1452342648751915119/IMG_4531.gif?ex=694976fe&is=6948257e&hm=cc7f84cca832ca2dcd8c15d594fa6b4ca134dd7d92dfc7276cb11a58bf4f5eed" alt="Link me">
+</a>`;
+
+  navigator.clipboard.writeText(code);
+}
