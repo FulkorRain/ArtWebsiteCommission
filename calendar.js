08b65ccf1a24cc6a1e288b87e461728d17f3d726
FulkorRain <ranygamer353@gmail.com>
2026-02-11 20:25:52 +0300

    Changed calendar

--- calendar.js
+++ calendar.js
@@ -1,103 +1,125 @@
-monthnames = new Array(
-"January",
-"Februrary",
-"March",
-"April",
-"May",
-"June",
-"July",
-"August",
-"September",
-"October",
-"November",
-"Decemeber");
-var linkcount=0;
-function addlink(month, day, href) {
-var entry = new Array(3);
-entry[0] = month;
-entry[1] = day;
-entry[2] = href;
-this[linkcount++] = entry;
+class Calendar {
+    constructor(containerId) {
+        this.container = document.getElementById(containerId);
+        this.today = new Date();
+        this.currentMonth = this.today.getMonth();
+        this.currentYear = this.today.getFullYear();
+        this.currentDate = this.today.getDate();
+        
+        this.monthNames = [
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        
+        this.links = {};
+    }
+
+    addLink(monthIndex, day, url) {
+        const key = `${monthIndex}-${day}`;
+        this.links[key] = url;
+    }
+
+    getDaysInMonth(month, year) {
+        return new Date(year, month + 1, 0).getDate();
+    }
+
+    getFirstDayOfMonth(month, year) {
+        return new Date(year, month, 1).getDay();
+    }
+
+    render() {
+        if (!this.container) return;
+
+        const daysInMonth = this.getDaysInMonth(this.currentMonth, this.currentYear);
+        const startDay = this.getFirstDayOfMonth(this.currentMonth, this.currentYear);
+
+        const table = document.createElement('table');
+        table.className = 'calendar-widget';
+
+        const thead = document.createElement('thead');
+        const headerRow = document.createElement('tr');
+        const headerCell = document.createElement('td');
+        
+        headerCell.colSpan = 7;
+        headerCell.className = 'month-header box-label'; 
+        
+        headerCell.innerHTML = `<div class="month-label"><p>${this.monthNames[this.currentMonth]} ${this.currentYear}<p></div>`;
+        
+        headerRow.appendChild(headerCell);
+        thead.appendChild(headerRow);
+
+        const daysRow = document.createElement('tr');
+        const weekDays = ['Su', 'M', 'Tu', 'W', 'Th', 'F', 'Sa'];
+        
+        weekDays.forEach((day, index) => {
+            const th = document.createElement('td');
+            th.className = `week-day week-${index + 1}`;
+            th.textContent = day;
+            daysRow.appendChild(th);
+        });
+        thead.appendChild(daysRow);
+        table.appendChild(thead);
+
+        const tbody = document.createElement('tbody');
+        let dateCount = 1;
+        let row = document.createElement('tr');
+
+        for (let i = 0; i < startDay; i++) {
+            const emptyCell = document.createElement('td');
+            emptyCell.innerHTML = '&nbsp;';
+            row.appendChild(emptyCell);
+        }
+
+        for (let i = startDay; i < 7; i++) {
+            if (dateCount <= daysInMonth) {
+                row.appendChild(this.createDayCell(dateCount));
+                dateCount++;
+            } else {
+                break; 
+            }
+        }
+        tbody.appendChild(row);
+
+        while (dateCount <= daysInMonth) {
+            row = document.createElement('tr');
+            for (let i = 0; i < 7; i++) {
+                if (dateCount <= daysInMonth) {
+                    row.appendChild(this.createDayCell(dateCount));
+                    dateCount++;
+                } else {
+                    const emptyCell = document.createElement('td');
+                    emptyCell.innerHTML = '&nbsp;';
+                    row.appendChild(emptyCell);
+                }
+            }
+            tbody.appendChild(row);
+        }
+
+        table.appendChild(tbody);
+        
+        this.container.innerHTML = '';
+        this.container.appendChild(table);
+    }
+
+    createDayCell(day) {
+        const cell = document.createElement('td');
+        const linkKey = `${this.currentMonth}-${day}`;
+        
+        let content = day;
+        
+        if (day === this.currentDate) {
+            content = `<span class="current-date"><strong>${day}</strong></span>`;
+        }
+
+        if (this.links[linkKey]) {
+            const a = document.createElement('a');
+            a.href = this.links[linkKey];
+            a.innerHTML = content;
+            cell.appendChild(a);
+        } else {
+            cell.innerHTML = content;
+        }
+
+        return cell;
+    }
 }
-Array.prototype.addlink = addlink;
-linkdays = new Array();
-monthdays = new Array(12);
-monthdays[0]=31;
-monthdays[1]=28;
-monthdays[2]=31;
-monthdays[3]=30;
-monthdays[4]=31;
-monthdays[5]=30;
-monthdays[6]=31;
-monthdays[7]=31;
-monthdays[8]=30;
-monthdays[9]=31;
-monthdays[10]=30;
-monthdays[11]=31;
-todayDate=new Date();
-thisday=todayDate.getDay();
-thismonth=todayDate.getMonth();
-thisdate=todayDate.getDate();
-thisyear=todayDate.getYear();
-thisyear = thisyear % 100;
-thisyear = ((thisyear < 50) ? (2000 + thisyear) : (1900 + thisyear));
-if (((thisyear % 4 == 0)
-&& !(thisyear % 100 == 0))
-||(thisyear % 400 == 0)) monthdays[1]++;
-startspaces=thisdate;
-while (startspaces > 7) startspaces-=7;
-startspaces = thisday - startspaces + 1;
-if (startspaces < 0) startspaces+=7;
-document.write("<table border=2 bgcolor=white ");
-document.write("bordercolor=black><font color=black>");
-document.write("<tr><td class='month' colspan=7><center><strong>"
-+ monthnames[thismonth] + " " + thisyear
-+ "</strong></center></font></td></tr>");
-document.write("<tr>");
-document.write("<td align=center class='week-1'>Su</td>");
-document.write("<td align=center class='week-2'>M</td>");
-document.write("<td align=center class='week-3'>Tu</td>");
-document.write("<td align=center class='week-4'>W</td>");
-document.write("<td align=center class='week-5'>Th</td>");
-document.write("<td align=center class='week-6'>F</td>");
-document.write("<td align=center class='week-7'>Sa</td>");
-document.write("</tr>");
-document.write("<tr>");
-for (s=0;s<startspaces;s++) {
-document.write("<td> </td>");
-}
-count=1;
-while (count <= monthdays[thismonth]) {
-for (b = startspaces;b<7;b++) {
-linktrue=false;
-document.write("<td>");
-for (c=0;c<linkdays.length;c++) {
-if (linkdays[c] != null) {
-if ((linkdays[c][0]==thismonth + 1) && (linkdays[c][1]==count)) {
-document.write("<a href=\"" + linkdays[c][2] + "\">");
-linktrue=true;
-}
-}
-}
-if (count==thisdate) {
-document.write("<font color='FF0000' class='date'><strong>");
-}
-if (count <= monthdays[thismonth]) {
-document.write(count);
-}
-else {
-document.write(" ");
-}
-if (count==thisdate) {
-document.write("</strong></font>");
-}
-if (linktrue)
-document.write("</a>");
-document.write("</td>");
-count++;
-}
-document.write("</tr>");
-document.write("<tr>");
-startspaces=0;
-}
-document.write("</table></p>");
